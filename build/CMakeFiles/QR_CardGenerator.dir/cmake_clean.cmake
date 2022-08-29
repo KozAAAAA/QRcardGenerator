05e89e8203d8cdc076666bc4d080e987bbd9ac9f
KozAAAAA <mateusz-koza@outlook.com>
2022-08-29 19:07:45 +0200

    first push

---
+++ build/CMakeFiles/QR_CardGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QR_CardGenerator.dir/main.cpp.o"
+  "QR_CardGenerator"
+  "QR_CardGenerator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QR_CardGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
