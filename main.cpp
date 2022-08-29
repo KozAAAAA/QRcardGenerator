3c6f496f1fdbe5d5aaaf9ff17fa3d90201428a22
KozAAAAA <mateusz-koza@outlook.com>
2022-08-29 22:27:34 +0200

    file names reading works

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <filesystem>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[]) 
+{
+    const fs::path startPath{ fs::path("files/QRcodes/") };
+    std::vector<fs::path> files{};
+    std::copy_if(fs::directory_iterator(startPath), {}, std::back_inserter(files), [](const fs::directory_entry& de) { return de.path().extension() == ".svg"; });
+    for (const fs::path& p : files) std::cout << p.string() << '\n';
+    std::cout << files.size()<< "\n";
+
+    
+
+    return 0;
 }
