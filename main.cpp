9d5394fe42ad1386806f0964a7cd6a37338c7825
KozAAAAA <mateusz-koza@outlook.com>
2022-08-30 16:22:43 +0200

    restructured code

--- main.cpp
+++ main.cpp
@@ -10,37 +10,44 @@ int main(int argc, char *argv[])
     const fs::path QRcodes{"files/QRcodes"};
     const fs::path finalFiles{"files/finalFiles"};
     const fs::path baseSVG{"files/baseSVG/baseSVG.svg"};
-    std::ifstream inFile;
 
-    for (auto& path: fs::directory_iterator(finalFiles))
+    uint64_t numCreatedFiles{0};
+
+    for (auto &path : fs::directory_iterator(finalFiles))
     {
         fs::remove_all(path);
     }
 
-    for (auto const &dir_entry : fs::directory_iterator{QRcodes})
+    for (auto const &dirEntry : fs::directory_iterator{QRcodes})
     {
-        auto currentFilePath = dir_entry.path();
+        auto currentFilePath = dirEntry.path();
 
         if (currentFilePath.extension() == ".svg")
         {
-            inFile.open(baseSVG);
-            auto currentFileName = currentFilePath.filename();
-            //auto currentFileNameString = currentFileName.string();
 
+            auto currentFileName = currentFilePath.filename();
+
+            std::ifstream inFile;
             std::ofstream outFile;
-            outFile.open((finalFiles/currentFileName), std::ios::app);
+
+            inFile.open(baseSVG);
+            outFile.open((finalFiles / currentFileName), std::ios::app);
 
             std::string inFileLine;
             while (std::getline(inFile, inFileLine))
             {
                 inFileLine = std::regex_replace(inFileLine, std::regex("\\Name_Here"), currentFileName.replace_extension().string());
-                //std::cout<<inFileLine<< "\n";
                 outFile << inFileLine;
             }
-            outFile.close();
+
             inFile.close();
+            outFile.close();
+            numCreatedFiles++;
         }
     }
+
+    std::cout << "Done\n";
+    std::cout << "Created " << numCreatedFiles << " files.\n";
 
     return 0;
 }
